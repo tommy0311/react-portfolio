92bea5060eb0fc974b4b7b2982d3ac6af160d69f
litangchen <chen.li.tang@gmail.com>
2022-08-13 07:17:54 +0800

    refactor: replace class to function components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,98 +1,93 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Typical from "react-typical";
 import Switch from "react-switch";
 
-class Header extends Component {
-  titles = [];
+function Header(props) {
+  let name = "";
+  let titles = [];
 
-  constructor() {
-    super();
-    this.state = { checked: false };
-    this.onThemeSwitchChange = this.onThemeSwitchChange.bind(this);
+  const [checked, setChecked] = useState(false);
+
+  function onThemeSwitchChange(checked) {
+    setChecked(checked);
+    setTheme();
   }
 
-  onThemeSwitchChange(checked) {
-    this.setState({ checked });
-    this.setTheme();
-  }
-
-  setTheme() {
-    var dataThemeAttribute = "data-theme";
-    var body = document.body;
-    var newTheme =
+  function setTheme() {
+    const dataThemeAttribute = "data-theme";
+    const body = document.body;
+    const newTheme =
       body.getAttribute(dataThemeAttribute) === "dark" ? "light" : "dark";
     body.setAttribute(dataThemeAttribute, newTheme);
   }
 
-  render() {
-    if (this.props.sharedData) {
-      var name = this.props.sharedData.name;
-      this.titles = this.props.sharedData.titles.map(x => [ x.toUpperCase(), 1500 ] ).flat();
-    }
+  if (props.sharedData) {
+    name = props.sharedData.name;
+    titles = props.sharedData.titles.map(x => [x.toUpperCase(), 1500]).flat();
+  }
 
-    const HeaderTitleTypeAnimation = React.memo( () => {
-      return <Typical className="title-styles" steps={this.titles} loop={50} />
-    }, (props, prevProp) => true);
+  const HeaderTitleTypeAnimation = React.memo(() => {
+    return <Typical className="title-styles" steps={titles} loop={50} />
+  }, (props, prevProp) => true);
 
-    return (
-      <header id="home" style={{ height: window.innerHeight - 140, display: 'block' }}>
-        <div className="row aligner" style={{height: '100%'}}>
-          <div className="col-md-12">
-            <div>
-              <span className="iconify header-icon" data-icon="la:laptop-code" data-inline="false"></span>
-              <br/>
-              <h1 className="mb-0">
-                <Typical steps={[name]} wrapper="p" />
-              </h1>
-              <div className="title-container">
-                <HeaderTitleTypeAnimation />
-              </div>
-              <Switch
-                checked={this.state.checked}
-                onChange={this.onThemeSwitchChange}
-                offColor="#baaa80"
-                onColor="#353535"
-                className="react-switch mx-auto"
-                width={90}
-                height={40}
-                uncheckedIcon={
-                  <span
-                    className="iconify"
-                    data-icon="twemoji:owl"
-                    data-inline="false"
-                    style={{
-                      display: "block",
-                      height: "100%",
-                      fontSize: 25,
-                      textAlign: "end",
-                      marginLeft: "20px",
-                      color: "#353239",
-                    }}
-                  ></span>
-                }
-                checkedIcon={
-                  <span
-                    className="iconify"
-                    data-icon="noto-v1:sun-with-face"
-                    data-inline="false"
-                    style={{
-                      display: "block",
-                      height: "100%",
-                      fontSize: 25,
-                      textAlign: "end",
-                      marginLeft: "10px",
-                      color: "#353239",
-                    }}
-                  ></span>
-                }
-                id="icon-switch"
-              />
+  return (
+    <header id="home" style={{ height: window.innerHeight - 140, display: 'block' }}>
+      <div className="row aligner" style={{ height: '100%' }}>
+        <div className="col-md-12">
+          <div>
+            <span className="iconify header-icon" data-icon="la:laptop-code" data-inline="false"></span>
+            <br />
+            <h1 className="mb-0">
+              <Typical steps={[name]} wrapper="p" />
+            </h1>
+            <div className="title-container">
+              <HeaderTitleTypeAnimation />
             </div>
+            <Switch
+              checked={checked}
+              onChange={onThemeSwitchChange}
+              offColor="#baaa80"
+              onColor="#353535"
+              className="react-switch mx-auto"
+              width={90}
+              height={40}
+              uncheckedIcon={
+                <span
+                  className="iconify"
+                  data-icon="twemoji:owl"
+                  data-inline="false"
+                  style={{
+                    display: "block",
+                    height: "100%",
+                    fontSize: 25,
+                    textAlign: "end",
+                    marginLeft: "20px",
+                    color: "#353239",
+                  }}
+                ></span>
+              }
+              checkedIcon={
+                <span
+                  className="iconify"
+                  data-icon="noto-v1:sun-with-face"
+                  data-inline="false"
+                  style={{
+                    display: "block",
+                    height: "100%",
+                    fontSize: 25,
+                    textAlign: "end",
+                    marginLeft: "10px",
+                    color: "#353239",
+                  }}
+                ></span>
+              }
+              id="icon-switch"
+            />
           </div>
         </div>
-      </header>
-    );
-  }
+      </div>
+    </header>
+  );
 }
 
 export default Header;
