59274ded0268a6ff0be9f775e8a572cfc41b6ebd
litangchen <chen.li.tang@gmail.com>
2022-08-13 11:23:44 +0800

    feat: add routes/login.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from "./routes/login";
 
 /* GLOBAL VARIABLES */
 
@@ -12,5 +15,13 @@ window.$secondaryLanguage = 'pl';
 window.$primaryLanguageIconId = 'primary-lang-icon';
 window.$secondaryLanguageIconId = 'secondary-lang-icon';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="login" element={<Login />} />
+    </Routes>
+  </BrowserRouter>
+), document.getElementById('root'));
+
 serviceWorker.register();
---
+++ src/routes/login.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Login() {
+  return (
+    <div className="login-container">
+      <div className="card">
+        <img id="profile-img" class="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" alt="avatar" />
+        <p id="profile-name" class="profile-name-card"></p>
+        <form className="form-signin">
+          <span id="reauth-email" className="reauth-email"></span>
+          <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autofocus />
+          <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
+          <button className="btn btn-lg btn-primary btn-block btn-signin" type="submit">Sign in</button>
+        </form>
+      </div>
+    </div>
+  );
+}
