aeaf461577b8fd39cd8c6fb5fc231cb433f01177
TommyChen <chen.li.tang@gmail.com>
2022-10-01 12:37:39 +0800

    Updates

---
+++ precache-manifest.14b30885bd40a498890743f1b52b0287.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2895b358349345562d6539256caa77ac",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "fc94e6530fb9de739049",
+    "url": "/react-portfolio/static/css/2.3ae0c06b.chunk.css"
+  },
+  {
+    "revision": "5df19937e0a5eb74b22b",
+    "url": "/react-portfolio/static/css/main.70c87a9b.chunk.css"
+  },
+  {
+    "revision": "fc94e6530fb9de739049",
+    "url": "/react-portfolio/static/js/2.cf601298.chunk.js"
+  },
+  {
+    "revision": "123708f4380f7d3bb17f454ef345e2cc",
+    "url": "/react-portfolio/static/js/2.cf601298.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5df19937e0a5eb74b22b",
+    "url": "/react-portfolio/static/js/main.2720cdf1.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
