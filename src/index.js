7eb8dd72c69b789ca3da6fc46ba56bf657170ede
TommyChen <chen.li.tang@gmail.com>
2022-10-01 13:23:29 +0800

    chore: change BrowserRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import * as ReactDOM from 'react-dom/client';
 import './index.scss';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./routes/login";
 import Edit from "./routes/edit";
@@ -27,8 +27,7 @@ const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 root.render(
-
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="edit" element={<Provider store={store}><ProtectedLayout /></Provider>} >
         <Route path="" element={<h1>404 Not Found</h1>} />
@@ -54,7 +53,7 @@ root.render(
 
       <Route path="*" element={<h1>404 Not Found</h1>} />
     </Routes>
-  </BrowserRouter >
+  </HashRouter >
 )
 
 serviceWorker.register();
