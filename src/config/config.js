1b9596c2a443c62eff16c3a0e6562bfe3760708c
TommyChen <chen.li.tang@gmail.com>
2022-10-02 10:55:51 +0800

    chore: change api server url

--- src/config/config.js
+++ src/config/config.js
@@ -1,1 +1,1 @@
-export const REACT_APP_APISERVER_BASE_URL = process.env.REACT_APP_APISERVER_BASE_URL || "https://xmnubwdice.eu09.qoddiapp.com"
+export const REACT_APP_APISERVER_BASE_URL = process.env.REACT_APP_APISERVER_BASE_URL || "https://yotimrkfuh.us12.qoddiapp.com"
